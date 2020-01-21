ef257057e53c6f1132256ddd1a5ecaed42592123
Toni Kuosmanen <toni.t.kuosmanen@gmail.com>
2020-01-21 09:36:03 +0200

    Add activities

--- app/src/main/java/com/example/assistedreminder/MainActivity.kt
+++ app/src/main/java/com/example/assistedreminder/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.example.assistedreminder
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var fabOpened = false
+
+        floatingActionButton_ShowActivity.setOnClickListener{
+            if(!fabOpened){
+                fabOpened = true
+                floatingActionButton_TimeActivity.animate().translationY(-resources.getDimension(R.dimen.standard_66))
+                floatingActionButton_MapActivity.animate().translationY(-resources.getDimension(R.dimen.standard_116))
+            }else{
+                fabOpened = false
+                floatingActionButton_TimeActivity.animate().translationY(0f)
+                floatingActionButton_MapActivity.animate().translationY(0f)
+            }
+        }
+
+        buttonToast.setOnClickListener{
+            Toast.makeText(applicationContext, "A Fine Toast!" , Toast.LENGTH_SHORT)
+                .show()
+        }
+        floatingActionButton_TimeActivity.setOnClickListener{
+            startActivity(Intent(applicationContext, TimeActivity::class.java))
+        }
+        floatingActionButton_MapActivity.setOnClickListener{
+            startActivity(Intent(applicationContext, MapActivity::class.java))
+        }
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/assistedreminder/MapActivity.kt
@@ -0,0 +1,14 @@
+package com.example.assistedreminder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+
+    }
+}
